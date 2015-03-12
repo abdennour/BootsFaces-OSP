996795ea316d388f01a5f67b354a2eb2f365f111
abdennour <abdennour.toumi@gmail.com>
2015-03-12 14:01:32 +0300

    Create TimeLine.java
    POC:  http://thevectorlab.net/flatlab/ 
    find timeline component

---
+++ src/net/bootsfaces/component/TimeLine.java
@@ -0,0 +1,41 @@
+/**
+* Copyright 2014 Abdennour TOUMI (Abdennour.tm)
+*
+* This file is part of BootsFaces.
+*
+* BootsFaces is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Lesser General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* BootsFaces is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU Lesser General Public License for more details.
+*
+* You should have received a copy of the GNU Lesser General Public License
+* along with BootsFaces. If not, see <http://www.gnu.org/licenses/>.
+*/
+package net.bootsfaces.component;
+import java.io.IOException;
+import java.util.Map;
+import javax.faces.application.ResourceDependencies;
+import javax.faces.application.ResourceDependency;
+import javax.faces.component.FacesComponent;
+import javax.faces.component.UIComponentBase;
+import javax.faces.context.FacesContext;
+import javax.faces.context.ResponseWriter;
+import net.bootsfaces.listeners.AddResourcesListener;
+import net.bootsfaces.render.A;
+import net.bootsfaces.C;
+import net.bootsfaces.render.H;
+import net.bootsfaces.render.R;
+import net.bootsfaces.render.RdropButton;
+/**
+*
+* @author abdennour.tm
+*/
+
+public class TimeLine  {
+
+}
